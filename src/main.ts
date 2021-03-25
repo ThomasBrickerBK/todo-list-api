4ee57cbef18c012ca27c0ebc44c65dee83908e10
Tomas Bricker <thomadbricker87@gmail.com>
2021-03-25 15:46:44 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: '*'});
+
   app.setGlobalPrefix('api');
 
   const options = new DocumentBuilder()
